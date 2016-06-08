c0541d7e57b699460c3d85c7255f6dfc6bce3515
coumbsek <coumbs.ek@gmail.com>
2016-06-09 01:52:05 +0200

    ajout d'un fichier contenant les diverses contantes necessaires aux programmes, identifiants, données de connexion, etc souvent empiriques

---
+++ constantes.h
@@ -0,0 +1,14 @@
+#ifndef CONSTANTES_H
+#define CONSTANTES_H
+
+#define ADDR_ANNUAIRE "localhost"
+#define PORT_ANNUAIRE "8080"
+
+#define NBCLIENTS 3
+#define NBSERVERS 2
+
+#define ID_SERVER 0x00F0
+#define ID_CLIENT 0x0F00
+#define ID_ANNUAIRE 0xF000
+
+#endif
